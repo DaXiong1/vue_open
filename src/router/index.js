3ecf172753827b963b3af55d74f0489e631188a0
DaXiong1 <zhang_sx09@163.com>
2018-03-15 15:35:00 +0800

    vue-router添加mode配置，而且使用懒加载配置页面组件，提高首屏加载速度

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import IndexZh from '@/components/index_zh'
-import Download from '@/components/download'
+/*
 import Index from '@/components/index'
 import IdCardCheck from '@/components/idCardCheck'
 import UserCardInfo from '@/components/userCardInfo'
 import DictionarySelect from '@/components/dictionarySelect'
 import IdentitySupplement from '@/components/identitySupplement'
 import ProvinceCitySelect from '@/components/provinceCitySelect'
-import CountySelect from '@/components/countySelect'
+import CountySelect from '@/components/countySelect' */
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     // 首页
     {
@@ -24,49 +25,50 @@ export default new Router({
     {
       path: '/download',
       name: 'download',
-      component: Download
+      // 使用懒加载的方式配置其他页面，提高首页加载速度
+      component: resolve => require(['@/components/download.vue'], resolve)
     },
     // 业务页面1
     {
       path: '/index',
       name: 'index',
-      component: Index
+      component: resolve => require(['@/components/index.vue'], resolve)
     },
     // 业务页面2
     {
       path: '/idCardCheck',
       name: 'idCardCheck',
-      component: IdCardCheck
+      component: resolve => require(['@/components/idCardCheck.vue'], resolve)
     },
     // 业务页面3
     {
       path: '/userCardInfo',
       name: 'userCardInfo',
-      component: UserCardInfo
+      component: resolve => require(['@/components/userCardInfo.vue'], resolve)
     },
     // 业务页面3-1
     {
       path: '/dictionarySelect',
       name: 'dictionarySelect',
-      component: DictionarySelect
+      component: resolve => require(['@/components/dictionarySelect.vue'], resolve)
     },
     // 业务页面4
     {
       path: '/identitySupplement',
       name: 'identitySupplement',
-      component: IdentitySupplement
+      component: resolve => require(['@/components/identitySupplement.vue'], resolve)
     },
     // 业务页面4-1
     {
       path: '/provinceCitySelect',
       name: 'provinceCitySelect',
-      component: ProvinceCitySelect
+      component: resolve => require(['@/components/provinceCitySelect.vue'], resolve)
     },
     // 业务页面4-1-1
     {
       path: '/countySelect',
       name: 'countySelect',
-      component: CountySelect
+      component: resolve => require(['@/components/countySelect.vue'], resolve)
     }
   ]
 })
